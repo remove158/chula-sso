f772a78b691820b1794801b670a0e9bb77cab7e4
plangton <remove158@gmail.com>
2022-11-08 06:37:45 +0700

    add: test

--- internal/services/auth_test.go
+++ internal/services/auth_test.go
@@ -3,6 +3,7 @@ package services_test
 import (
 	"testing"
 
+	"github.com/remove158/chula-sso/cmd/models"
 	"github.com/remove158/chula-sso/internal/services"
 	"github.com/stretchr/testify/suite"
 )
@@ -10,21 +11,81 @@ import (
 type AuthServiceTest struct {
 	suite.Suite
 	authService *services.AuthService
+	test_user   models.UserResponse
 }
 
 func TestAuthService(t *testing.T) {
 	authService := services.NewAuthService()
-	suite.Run(t, &AuthServiceTest{authService: authService})
-}
+	test_user := models.UserResponse{
+		UID:         "test",
+		Username:    "test",
+		GECOS:       "test",
+		Disable:     false,
+		Roles:       []string{"test@test.com"},
+		FirstName:   "test",
+		FirstNameTH: "test",
+		LastName:    "test",
+		LastNameTH:  "test",
+		OUID:        "test",
+		Email:       "test",
+	}
 
-func (s *AuthServiceTest) TestGetLoginSuccess() {
-	// s.Equal(nil, s.authService.UpdateUser("test"))
+	suite.Run(t, &AuthServiceTest{
+		authService: authService,
+		test_user:   test_user,
+	})
 }
 
 func (s *AuthServiceTest) TestPostLoginSuccess() {
-	// s.Equal("Hello", s.authService.PostLogin())
+
+	ticket := s.authService.PostLogin(s.test_user)
+	s.NotEmpty(ticket)
 }
 
 func (s *AuthServiceTest) TestServiceValidationSuccess() {
-	// s.Equal("Hello", s.authService.ServiceValidation("test"))
+	ticket := s.authService.PostLogin(s.test_user)
+	request := models.ServiceValidateRequest{
+		Ticket:       ticket,
+		DeeAppID:     "test",
+		DeeAppSecret: "test",
+	}
+	result, err := s.authService.ServiceValidation(request)
+	s.Equal(result.UID, s.test_user.UID)
+	s.NoError(err)
+}
+
+func (s *AuthServiceTest) TestServiceValidationFailOnDeeAppIDInValid() {
+	ticket := s.authService.PostLogin(s.test_user)
+	request := models.ServiceValidateRequest{
+		Ticket:       ticket,
+		DeeAppID:     "",
+		DeeAppSecret: "test",
+	}
+	result, err := s.authService.ServiceValidation(request)
+	s.Error(err)
+	s.Empty(result)
+}
+
+func (s *AuthServiceTest) TestServiceValidationFailOnDeeAppSecretInValid() {
+	ticket := s.authService.PostLogin(s.test_user)
+	request := models.ServiceValidateRequest{
+		Ticket:       ticket,
+		DeeAppID:     "test",
+		DeeAppSecret: "",
+	}
+	result, err := s.authService.ServiceValidation(request)
+	s.Error(err)
+	s.Empty(result)
+}
+
+func (s *AuthServiceTest) TestServiceValidationFailOnTicketInvalid() {
+	ticket := "invalid-" + s.authService.PostLogin(s.test_user)
+	request := models.ServiceValidateRequest{
+		Ticket:       ticket,
+		DeeAppID:     "test",
+		DeeAppSecret: "test",
+	}
+	result, err := s.authService.ServiceValidation(request)
+	s.Error(err)
+	s.Empty(result)
 }
